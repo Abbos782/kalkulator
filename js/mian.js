b329d24a2482d9a8dd451178c85f4122168b765a
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-27 13:55:13 +0300

    add js

---
+++ js/mian.js
@@ -0,0 +1,22 @@
+  let ekranElement  = document.querySelector(".input")
+
+let ValueElement = document.querySelectorAll('td')
+
+document.querySelectorAll('td').forEach(function(td){
+  td.addEventListener('click',function(e){
+     let qiymat = e.target.innerText;
+     if(qiymat == "AC"){
+         ekranElement.value =""
+     }else if(qiymat == "="){
+         ekranElement.value = eval(ekranElement.value)
+
+     }
+     else{
+        ekranElement.value  += qiymat
+     }
+      
+
+  
+     
+  })
+})
