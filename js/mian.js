1094edea15764e043af58c0191c27502d1479821
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-29 17:19:42 +0300

    add

--- js/mian.js
+++ js/mian.js
@@ -3,6 +3,10 @@ let temp = false;
 let inputElemnt = document.querySelector(".input")
 function tugma(obj){
   let sum = obj.innerText;
+  let a = inputElemnt.innerHTML.substr(
+    inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+  );
+  
   
   if(sum =="="){
     inputElemnt.innerHTML =eval(inputElemnt.innerHTML);
@@ -12,41 +16,26 @@ function tugma(obj){
     temp = false;
   }else{
     if(inputElemnt.innerHTML == "0"){
-      inputElemnt.innerHTML=sum;
+      inputElemnt.innerHTML+=sum;
     }
+  
     else{
-      if(inputElemnt.innerHTML.substring(
-        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
-      ) == "+" && sum == "+"){
+         
+      if(a == "+" && sum == "+" || a == "+" && sum == "/" ||  a == "+" && sum == "%" ||  a == "+" && sum == "*" ||  a == "+" && sum == "-" || a== "+" && sum=="."){
             temp = true;
-      } 
-      else if(inputElemnt.innerHTML.substring(
-        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
-      ) == "*" && sum == "*"){
-         temp =true;
-      }
-      else if(inputElemnt.innerHTML.substring(
-        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
-      ) == "%" && sum == "%"){
-         temp =true;
-      }
-      else if(inputElemnt.innerHTML.substring(
-        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
-      ) == "/" && sum == "/"){
-         temp =true;
-      }
-      else if(inputElemnt.innerHTML.substring(
-        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
-      ) == "-" && sum == "-"){
-         temp =true;
-      }
-      else if(inputElemnt.innerHTML.substring(
-        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
-      ) == "." && sum == "."){
-         temp =true;
-      }
+         } else if(a == "-" && sum == "+" || a == "-" && sum == "/" ||  a == "-" && sum == "%" ||  a == "-" && sum == "*" ||  a == "-" && sum == "-" || a== "-" && sum=="."){
+        temp = true;
+         }else if(a == "*" && sum == "+" || a == "*" && sum == "/" ||  a == "*" && sum == "%" ||  a == "*" && sum == "*" ||  a == "*" && sum == "-" || a== "*" && sum=="."){
+          temp = true;
+         }else if(a == "/" && sum == "+" || a == "/" && sum == "/" ||  a == "/" && sum == "%" ||  a == "/" && sum == "*" ||  a == "/" && sum == "-" || a== "/" && sum=="."){
+          temp = true;
+         }else if(a == "%" && sum == "+" || a == "%" && sum == "/" ||  a == "%" && sum == "%" ||  a == "%" && sum == "*" ||  a == "%" && sum == "-" || a== "%" && sum=="."){
+          temp = true;
+        }else if(a == "." && sum == "+" || a == "." && sum == "/" ||  a == "." && sum == "%" ||  a == "." && sum == "*" ||  a == "." && sum == "-" || a== "." && sum=="."){
+          temp = true;
+        }
       
-      else{
+    else{
         temp = false;
       } 
 
