7c91b01826a90ee8404fd4d2df9da0b9b66d5749
Abbos782 <77330418+Abbos782@users.noreply.github.com>
2021-03-29 16:10:13 +0300

    add js

--- js/mian.js
+++ js/mian.js
@@ -1,16 +1,58 @@
+let temp = false;
+
 let inputElemnt = document.querySelector(".input")
 function tugma(obj){
   let sum = obj.innerText;
   
   if(sum =="="){
     inputElemnt.innerHTML =eval(inputElemnt.innerHTML);
+    temp = false;
   }else if(sum =="AC"){
     inputElemnt.innerHTML =""
+    temp = false;
   }else{
-    if(inputElemnt.innerHTML=="0"){
+    if(inputElemnt.innerHTML == "0"){
       inputElemnt.innerHTML=sum;
-    }else{
-      inputElemnt.innerHTML+=sum;
+    }
+    else{
+      if(inputElemnt.innerHTML.substring(
+        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+      ) == "+" && sum == "+"){
+            temp = true;
+      } 
+      else if(inputElemnt.innerHTML.substring(
+        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+      ) == "*" && sum == "*"){
+         temp =true;
+      }
+      else if(inputElemnt.innerHTML.substring(
+        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+      ) == "%" && sum == "%"){
+         temp =true;
+      }
+      else if(inputElemnt.innerHTML.substring(
+        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+      ) == "/" && sum == "/"){
+         temp =true;
+      }
+      else if(inputElemnt.innerHTML.substring(
+        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+      ) == "-" && sum == "-"){
+         temp =true;
+      }
+      else if(inputElemnt.innerHTML.substring(
+        inputElemnt.innerHTML.length - 1, inputElemnt.innerHTML.length
+      ) == "." && sum == "."){
+         temp =true;
+      }
+      
+      else{
+        temp = false;
+      } 
+
+      if(!temp) inputElemnt.innerHTML +=sum;
+
+      
     }
   }
   
